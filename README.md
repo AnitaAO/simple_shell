9b6d11245649cc3d42b49462b3858703216ba350
AnitaAO <anitaisbeta@gmail.com>
2023-10-15 10:00:52 -0700

    _atoi.c

---
+++ README.md
@@ -0,0 +1,21 @@
+## 0x16. C - Simple Shell
+# Tasks
+0. Betty would be proud
+- Write a beautiful code that passes the Betty checks
+1. Simple shell 0.1
+- Write a UNIX command line interpreter.
+2. Simple shell 0.2
+# Simple shell 0.1 +
+- Handle command lines with arguments
+3. Simple shell 0.3
+# Simple shell 0.2 +
+- Handle the PATH
+- fork must not be called if the command doesn’t exist
+4. Simple shell 0.4
+# Simple shell 0.3 +
+- Implement the exit built-in, that exits the shell
+- Usage: exit
+- You don’t have to handle any argument to the built-in exit
+5. Simple shell 1.0
+# Simple shell 0.4 +
+- Implement the env built-in, that prints the current environment
---
+++ _atoi.c
@@ -0,0 +1,76 @@
+#include "shell.h"
+
+/**
+ * interactive - returns true if shell is interactive mode
+ * @info: struct address
+ *
+ * Return: 1 if interactive, 0 otherwise
+ */
+
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - check if character is a delimeter
+ * @c: the character to be checked
+ * @delim: delimeter string
+ * Return: 1 if true, 0 if false
+ */
+
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ *_isalpha - check for alphabetic character
+ *@c: Character to inputted
+ *Return: 1 if c is true, 0 if otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ *_atoi - converts string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, otherwise, converted number
+ */
+
+int _atoi(char *s)
+{
+	int a, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (a = 0;  s[a] != '\0' && flag != 2; a++)
+	{
+		if (s[a] == '-')
+			sign *= -1;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[a] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
